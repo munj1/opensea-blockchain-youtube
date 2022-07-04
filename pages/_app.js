84c26aecc7c69acded0f9d5e4de731556d56e9e8
이홍준 <hongjun@ihongjuns-MacBook-Air.local>
2022-07-04 11:27:01 +0900

    minor change

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 
+// Testing Git pull
+
+
 /**
  * The chain ID 4 represents the Rinkeby network
  * The `injected` connector is a web3 connection method used by Metamask
